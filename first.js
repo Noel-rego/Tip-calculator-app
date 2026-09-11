b1f49af9074a7f846afd2f791c530a44ce2c00a3
Noel Rego <20leonarego@gmail.com>
2026-09-12 00:21:57 +0530

    query selecting id's

---
+++ first.js
@@ -0,0 +1,26 @@
+const fivetip=document.querySelector("#tip-5");
+const tentip=document.querySelector("#tip-10");
+const fifteentip=document.querySelector("#tip-15");
+const twentyfivetip=document.querySelector("#tip-25");
+const fiftytip=document.querySelector("#tip-50");
+const customtip=document.querySelector("#custom");
+
+const bill=document.querySelector("#bill");
+const noofpep=document.querySelector("#noofpep");
+
+
+const value=document.querySelector("#value");
+const value2=document.querySelector("#value2");
+const reset=document.querySelector("#reset");
+
+const getvalue=()=>{
+    billvalue=bill.value;
+    noofpepvalue=noofpep.value;
+    console.log(billvalue,noofpepvalue);
+}
+
+
+
+
+
+
