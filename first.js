e77fd6d265d3831abcd0337b87ceb32ef104544b
Noel Rego <20leonarego@gmail.com>
2026-09-20 13:45:53 +0530

    adding logic for custom tip input

--- first.js
+++ first.js
@@ -15,36 +15,46 @@ const reset=document.querySelector("#reset");
 const getvalue=()=>{
     billvalue=Number(bill.value);
     noofpepvalue=Number(noofpep.value);
+    customtipvalue=(Number(customtip.value))/100;
     if (noofpepvalue===0){
         noofpep.style.border="2px solid hsl(10, 100%, 50%)";
         error.style.display="block";
         noofpep.style.borderRadius="3%";
-
+        //for adding style from css
+        noofpep.classList.add("input-error");
     }
     else{
         console.log(`The bill costed ${billvalue} and theres ${noofpepvalue} people`);
         calctip();
         gettotol();
+    }   
+}
+const calctip=()=>{
+    if (customtipvalue==0){
+        const tipvalue=billvalue*(tipperc);
+        const tipvalueperp=(tipvalue/noofpepvalue).toFixed(2);
+        console.log(`tip perperson is ${tipvalueperp}`);
+        value.innerText=(`$${tipvalueperp}`);
+        return tipvalue;
+    }
+    else{
+        const tipvalue=billvalue*(customtipvalue);
+        const tipvalueperp=(tipvalue/noofpepvalue).toFixed(2);
+        console.log(`tip perperson is ${tipvalueperp}`);
+        value.innerText=(`$${tipvalueperp}`);
+        return tipvalue;
 
     }
     
 }
-const calctip=()=>{
-    const tipvalue=billvalue*(tipperc);
-    const tipvalueperp=(tipvalue/noofpepvalue);
-    console.log(`tip perperson is ${tipvalueperp}`);
-    value.innerText=(`$${tipvalueperp}`);
-    return tipvalue;
-}
 const gettotol=()=>{
     const totol=(billvalue)+Number(calctip());
-    const totolperp=(totol/noofpepvalue);
+    const totolperp=(totol/noofpepvalue).toFixed(2);
     console.log(`totol perperson is ${totolperp}`);
     value2.innerText=(`$${totolperp}`);
     reset.style.backgroundColor="hsl(183, 80%, 61%)";
     reset.style.color="hsl(183, 100%, 15%)";
 }
-
 const resetbtn=()=>{
     tipperc=0;
     billvalue=0;
@@ -63,17 +73,13 @@ const resetbtn=()=>{
     reset.style.backgroundColor = "hsl(186, 46%, 29%)";
     reset.style.color = "hsl(185, 10%, 45%)";
     
-    console.log("App Reset successfully!");
-
-
-    
+    console.log("App Reset successfully!");  
 }
 noofpep.addEventListener("change",getvalue,);
 tips.forEach((tip)=>{
     tip.addEventListener("click",(e)=>{
         tipperc=e.target.dataset.tip;
     });
-
 })
 reset.addEventListener("click",resetbtn);
 
